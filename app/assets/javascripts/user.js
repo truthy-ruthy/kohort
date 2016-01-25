4cbf1be13fb10d915098f8450b1de0321a3e7af1
Ruth Thatcher <runic77@gmail.com>
2016-01-25 10:55:29 -0600

    updating

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -3,38 +3,37 @@ $(document).ready(function () {
 $("#newUser").click(function(){
   $("h1").text("Registration");
   $(".logo").css({
-    "width":"120px",
-    "height":"120px",
+    "width":"75px",
+    "height":"75px",
     "top":"10px"
   });
-  $("#login-form").fadeOut(200);
-  $("#registration-form").delay(300).fadeIn(500);
+  $("#login-form").fadeOut(150);
+  $("#registration-form").delay(150).fadeIn(200);
   $(".other-options").fadeOut(200);
 });
 
-$("#signup-btn,#getpass-btn").click(function(){
-  $("h1").text("Log in");
-  $(".logo").css({
-    "width":"75px",
-    "height":"75px",
-    "top":"30px"
-  });
-
-  $("#registration-form,#fpass-form").fadeOut(200);
-  $("#login-form").delay(300).fadeIn(500);
-  $(".other-options").fadeIn(300);
-});
+// $("#signup-btn,#getpass-btn").click(function(){
+//   $("h1").text("Log in");
+//   $(".logo").css({
+//     "width":"75px",
+//     "height":"75px",
+//     "top":"10px"
+//   });
+//   // $("#registration-form,#fpass-form").fadeOut(150);
+//   // $("#login-form").delay(200).fadeIn(200);
+//   // $(".other-options").fadeIn(200);
+// });
 
 $("#fPass").click(function(){
   $("h1").text("Forgotten password");
   $(".logo").css({
     "width":"75px",
     "height":"75px",
-    "top":"40px"
+    "top":"10px"
   });
 
-  $("#login-form").fadeOut(200);
-  $("#fpass-form").delay(300).fadeIn(500);
+  $("#login-form").fadeOut(150);
+  $("#fpass-form").delay(200).fadeIn(200);
   $(".other-options").fadeOut(200);
 });
 
