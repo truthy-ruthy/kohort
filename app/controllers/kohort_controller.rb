e23b2a7b9ac8c05e810ba5caf4adbeff647c4173
Ruth Thatcher <runic77@gmail.com>
2016-01-26 20:02:52 -0600

    starting two panel layout

--- app/controllers/kohort_controller.rb
+++ app/controllers/kohort_controller.rb
@@ -1,29 +1,34 @@
 class KohortController < ApplicationController
 
+#shows kohorts that user belongs to
   def index
      @kohorts = Kohort.all
   end
 
+# gets new kohort form
   def new
     @kohort= Kohort.new
   end
 
+#find kohort with:
+# must match: zip code, age(not visible), field, conversation_level
+# match or doesn't matter: coed, frequency, daytime
+#3 or less people
+#if match, add to kohort
+#display clickable on page if 4 people total, otherwise "pending kohort" in slot
+#else
+
+#creates kohort if one matching user requirements is not found
   def create
-    @kohort= Kohort.new(params[:kohort])
-    p params
-    if @kohort.save
+    # @kohort= Kohort.new(params[:kohort])
+    # p params
+    # if @kohort.save
 
     else
-      render 'new'
+      # render 'new'
     end
   end
 
 
 
-
-  # def leave_kohort
-  # end
-  #
-  # def kohort_home
-  # end
 end
